57ca031ec4f215bf5910235d291d3246797798f4
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2021-06-04 11:01:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -22,13 +22,26 @@ var ball = {
 }
 
 function setup(){
-  var canvas =  createCanvas(700,600);
+   var canvas =  createCanvas(700,600);
+   canvas.parent('canvas'); 
+    
+   video = createCapture(VIDEO);
+   video.size(700, 600);
+    
+   poseNet = ml5.poseNet(video, modelLoaded);  
+}
+
+function modelLoaded ()
+{
+    console.log('Pose Net is initialized');
 }
 
 
 function draw(){
 
  background(0); 
+    
+ image(video, 0, 0, 700, 600);   
 
  fill("black");
  stroke("black");
