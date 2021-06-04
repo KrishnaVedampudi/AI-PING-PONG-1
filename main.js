429950d272687914cf7aa39c820b1035c315ff1c
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2021-06-04 11:43:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -23,11 +23,11 @@ var ball = {
 
 function setup(){
    var canvas =  createCanvas(700,600);
-   canvas.parent('canvas');
-   
+   canvas.parent('canvas');     
     
    video = createCapture(VIDEO);
    video.size(700, 600);
+   video.hide();
     
    poseNet = ml5.poseNet(video, modelLoaded);  
 }
