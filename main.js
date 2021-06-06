95cd096c3c2cd2944d5c3753b0d7628317fa8608
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2021-06-06 10:02:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -29,7 +29,18 @@ function setup(){
    video.size(700, 600);
    video.hide();
     
-   poseNet = ml5.poseNet(video, modelLoaded);  
+   poseNet = ml5.poseNet(video, modelLoaded);
+   poseNet.on('pose', gotPoses) 
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+    {      
+    RightWristX = results[0].pose.nose.x;
+    RightWirstY = results[0].pose.nose.y;        
+    console.log(results);
+    }  
 }
 
 function modelLoaded ()
@@ -79,6 +90,10 @@ function draw(){
    
    //function move call which in very important
     move();
+    
+    fill("red");
+    stroke("red");
+    circle(RightWristX, RightWristY, 25);
 }
 
 
