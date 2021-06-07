db6053b704b3687a6093e5bb0efb266f8b2310db
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2021-06-07 13:42:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ var paddle2Y = 685,paddle2Height = 70;
 var score1 = 0, score2 =0;
 var paddle1Y;
 
+var scoreRightWrist;
+var RightWristX;
+var RightWristY;
+
 var  playerscore =0;
 var audio1;
 var pcscore =0;
